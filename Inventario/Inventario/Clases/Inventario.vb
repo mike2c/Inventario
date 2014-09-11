2c0b70605dcefa88970126195694251f7c87bafe
mike2c <mikedosce1992@gmail.com>
2014-09-11 15:16:22 -0600

    Formulario de Articulos programado,Falta programar el de Proveedor

---
+++ Inventario/Inventario/Clases/Inventario.vb
@@ -0,0 +1,39 @@
+﻿Public Class Inventario
+    Private id As Integer
+    Private exist As Integer
+    Private min As Integer
+
+    Sub New()
+
+    End Sub
+
+    Public Property IDInventario As Integer
+        Get
+            Return id
+        End Get
+        Set(value As Integer)
+            id = value
+        End Set
+    End Property
+
+    Public Property Existencia As Integer
+        Get
+            Return exist
+        End Get
+        Set(value As Integer)
+            exist = value
+        End Set
+    End Property
+
+    Public Property Minimo As Integer
+        Get
+            Return min
+        End Get
+        Set(value As Integer)
+            min = value
+        End Set
+    End Property
+
+
+
+End Class
